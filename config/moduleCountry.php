983e8d037276d4029b770a7ec825dd761649383e
Jomon Johnson <jomon.entero@gmail.com>
2019-06-07 16:59:56 +0530

    init

--- config/moduleCountry.php
+++ config/moduleCountry.php
@@ -1,9 +1,14 @@
 <?php
 
+use codexten\yii\base\Module;
+
 return [
+    'aliases' => [
+        '@moduleCountry' => '@codexten/yii/modules/country',
+    ],
     'modules' => [
         'country' => [
-            'class' => \codexten\yii\base\Module::class,
+            'class' => Module::class,
             'controllerNamespace' => 'codexten\yii\modules\country\controllers',
         ],
     ],

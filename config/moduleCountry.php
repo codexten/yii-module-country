5d09dcd1f3cbfa87545ea3ae9c1f2aeae50af89d
aneenashaju <aneenashaju@gmail.com>
2019-07-12 15:08:13 +0530

    country

--- config/moduleCountry.php
+++ config/moduleCountry.php
@@ -2,6 +2,8 @@
 
 use codexten\yii\base\Module;
 use codexten\yii\modules\country\controllers\CountryController;
+use codexten\yii\modules\country\models\Country;
+use codexten\yii\modules\country\models\search\CountrySearch;
 
 return [
     'modules' => [
@@ -11,6 +13,15 @@ return [
             'controllerMap' => [
                 'country' => [
                     'class' => CountryController::class,
+                    'modelClass' => Country::class,
+                    'newSearchModel' => function () {
+                        $className = 'codexten\yii\modules\country\models\search\CountrySearch';
+                        $searchModel = new $className();
+
+                        /* @var $searchModel CountrySearch */
+
+                        return $searchModel;
+                    },
                 ],
             ],
         ],
